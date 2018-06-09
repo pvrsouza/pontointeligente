ddb9c211c17dc33a5e52f3e4f059273be3c49eff
Pablo Souza <pvrsouza@gmail.com>
2018-06-08 22:04:53 -0300

    inclusao de classes de repositories

---
+++ src/main/java/br/com/pablovinicius/pontointeligente/repositories/EmpresaRepository.java
@@ -0,0 +1,13 @@
+package br.com.pablovinicius.pontointeligente.repositories;
+
+import br.com.pablovinicius.pontointeligente.entities.Empresa;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
+
+
+public interface EmpresaRepository extends JpaRepository<Empresa, Long> {
+	
+	@Transactional(readOnly = true)
+	Empresa findByCnpj(String cnpj);
+
+}
